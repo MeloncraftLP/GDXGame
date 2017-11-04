0fe497e7a603d9227d355f47cf024ef423b4380e
Meloncraft <michaelsmailbox-code@yahoo.de>
2017-11-04 17:35:54 +0100

    not Working
    Errors

--- GameGDX/core/src/com/melon/game/KeyHandler.java
+++ GameGDX/core/src/com/melon/game/KeyHandler.java
@@ -3,15 +3,37 @@ package com.melon.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 
 
-public class KeyHandler
+public class KeyHandler  implements InputProcessor
 {
 
-    public static void KeyHandlerMeth()
+    static void RunterFallen() //Runterfallen-Methode
     {
 
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Main.RunAnimationBool == false) //Pfeil-Rechts und nicht rennen
+        Main.RunterfallenBool = true; //Runterfallen(Kopie) true
+
+        if(Main.JumpTime > 0) //Wenn in der Luft
+        {
+
+            Main.JumpTime -= 40; //yCord(Kopie) -40(y)
+
+            //Alle Sprites -40(y) bewegen
+            Main.RunSprite.translateY(-40f);
+            Main.JumpSprite.translateY(-40f);
+//                    Main.GeschenkeSprite.translateY(-40f);
+            Main.IdleSprite.translateY(-40f);
+
+        }
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode)
+    {
+
+        if(keycode == Input.Keys.RIGHT && Main.RunAnimationBool == false) //Pfeil-Rechts und nicht rennen
         {
 
             Main.RunAnimationBool = true; //Rennen = true
@@ -22,11 +44,11 @@ public class KeyHandler
 //            Main.GeschenkeSprite.translateX(+12f);
             Main.IdleSprite.translateX(+12f);
 
-            Main.RunSprite.draw(Main.batch); //Laufanimation projezieren
+            Main.RunSprite.draw(Main.AnimationBatch); //Laufanimation projezieren
 
         }
 
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && Main.RunAnimationBool == false) //Pfeil-Links und nicht rennen
+        if(keycode == Input.Keys.LEFT && Main.RunAnimationBool == false) //Pfeil-Links und nicht rennen
         {
 
             Main.RunAnimationBool = true; //Rennen = true
@@ -44,11 +66,11 @@ public class KeyHandler
 
             }
 
-            Main.RunSprite.draw(Main.batch); //Laufanimation projezieren
+            Main.RunSprite.draw(Main.AnimationBatch); //Laufanimation projezieren
 
         }
 
-        if(Gdx.input.isKeyPressed(Input.Keys.UP) && Main.JumpAnimationBool == false) //Wenn Pfeil-Hochh und nicht springen
+        if(keycode == Input.Keys.UP && Main.JumpAnimationBool == false) //Wenn Pfeil-Hochh und nicht springen
         {
 
             Main.JumpAnimationBool = true; //Springen = true
@@ -76,21 +98,13 @@ public class KeyHandler
             if(Main.RunAnimationBool == false) //Schließlich wenn man nicht rennt:
             {
 
-                Main.JumpSprite.draw(Main.batch); //Sprunganimation projezieren
+                Main.JumpSprite.draw(Main.AnimationBatch); //Sprunganimation projezieren
 
             }
 
         }
-
-        else //Wenn man nicht die Sprungstaste drückt
-        {
-
-            KeyHandler.RunterFallen(); //Runterfallen
-
-        }
-
 /*
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && Main.GeschenkeAnimationBool == false)
+        if(keycode == Input.Keys.DOWN && Main.GeschenkeAnimationBool == false)
         {
 
             Main.GeschenkeAnimationBool = true;
@@ -100,44 +114,72 @@ public class KeyHandler
             Main.JumpSprite.translateX(-1f);
             Main.IdleSprite.translateX(-1f);
 
-//            Main.GeschenkeSprite.draw(Main.batch);
+//            Main.GeschenkeSprite.draw(Main.AnimationBatch);
 
         }
 
         */
 
-        if(Gdx.input.isKeyPressed(Input.Keys.ANY_KEY))
+        if(keycode != Input.Keys.ANY_KEY) //wenn keine Taste gedrückt wird
         {
+
+            Main.IdleSprite.draw(Main.AnimationBatch); //Idleanimation projezieren
 
         }
 
-        else //wenn keine Taste gedrückt wird:
-        {
-
-            Main.IdleSprite.draw(Main.batch); //Idleanimation projezieren
-
-        }
-
+        return true;
     }
 
-    static void RunterFallen() //Runterfallen-Methode
+    @Override
+    public boolean keyUp(int keycode)
     {
 
-        Main.RunterfallenBool = true; //Runterfallen(Kopie) true
-
-        if(Main.JumpTime > 0) //Wenn in der Luft
+        if(keycode == Input.Keys.UP)
         {
 
-            Main.JumpTime -= 40; //yCord(Kopie) -40(y)
-
-            //Alle Sprites -40(y) bewegen
-            Main.RunSprite.translateY(-40f);
-            Main.JumpSprite.translateY(-40f);
-//                    Main.GeschenkeSprite.translateY(-40f);
-            Main.IdleSprite.translateY(-40f);
+            KeyHandler.RunterFallen();
 
         }
 
+        return true;
+
     }
+
+    @Override
+    public boolean keyTyped(char character)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount)
+    {
+        return false;
+    }
+
 
 }
